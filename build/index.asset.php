654422af6b8dae328d5342dcc4810068f604d4c3
anomiex <anomiex@users.noreply.github.com>
2024-01-08 17:24:06 +0000

    Lock file maintenance (#34882)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7450800943

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '58dd44b48c4070487c17');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'e5a8549d0552bed2d8cd');
