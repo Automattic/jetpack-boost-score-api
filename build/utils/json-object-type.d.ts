4429c2ea627e8f6bea0d853722a4ea590f42412e
anomiex <anomiex@users.noreply.github.com>
2024-12-03 21:53:01 +0000

    Update various eslint plugins (#40437)
    Notable changes include:
    
    * `@typescript-eslint/ban-types` is split into some other rules,
      particularly `@typescript-eslint/no-unsafe-function-type`.
    * `@typescript-eslint/no-unused-vars` now detects when a variable is
      only used by `typeof`, which can often be replaced.
    * `@typescript-eslint/no-unused-vars` now flags `} catch ( e ) {` where
      the `e` isn't used in the catch block, as `} catch {` is allowed since
      ES2019.
    * `@typescript-eslint/no-empty-object-type` by default now flags
      `interface Foo extends Bar {}`. Update the config to turn that back
      off.
    * New rule `@typescript-eslint/no-require-imports` needs to be disabled
      for `.cjs` files.
    * New rule `@typescript-eslint/no-unused-expressions` can replace
      `no-unused-expressions` even for JS files, so let's do that to avoid
      having to separately configure both with `allowShortCircuit` and
      `allowTernary`.
    
    Also, added a pnpmfile hack so `@wordpress/eslint-plugin` won't bring in
    an older version of `@typescript-eslint/parser`.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12148571247
    
    Upstream-Ref: Automattic/jetpack@8e1d185bc756a0b8c52a96e1a159f3036dcdb4fa

--- build/utils/json-object-type.d.ts
+++ build/utils/json-object-type.d.ts
@@ -1,12 +1,10 @@
-import { z } from 'zod';
 /**
  * Definition for JSON types:
  * - JSONValue can be any value compatible with JSON; an object (containing JSONValues), array, string, number, boolean, or null
  * - JSONObject is an object containing JSONValues
  * - JSONSchema is a zod schema that can be used to validate JSONValues
  */
-declare const literalSchema: z.ZodUnion<[z.ZodString, z.ZodNumber, z.ZodBoolean, z.ZodNull]>;
-type Literal = z.infer<typeof literalSchema>;
+type Literal = string | number | boolean | null;
 export type JSONValue = Literal | JSONObject | JSONValue[];
 export type JSONObject = {
     [key: string]: JSONValue;
