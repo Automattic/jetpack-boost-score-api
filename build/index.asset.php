fdf9a7fb7d0126058f03604df867e19009b711a4
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-09-18 09:32:17 +0000

    Update dependency webpack to v5.101.3 (#45173)
    * Update dependency webpack to v5.101.3
    
    * Rename the file to fix the build error
    
    * Update test snapshots
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/17824008701
    
    Upstream-Ref: Automattic/jetpack@18eb749eec2185dc106f74b36c163ef458dded0f

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'e827c47192d715815dca');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'e6ad0ccb76589f5893c8');
