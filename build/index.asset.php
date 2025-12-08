8c71a052858fa5fe969079ce78de5f6a8ef36d8d
anomiex <anomiex@users.noreply.github.com>
2025-12-08 17:33:49 +0000

    Lock file maintenance (#46216)
    * Lock file maintenance
    * Redo lockfiles with composer 2.8.12
    * Remove unneeded changelog entries
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/20036583805
    
    Upstream-Ref: Automattic/jetpack@55a9910827f7534e5d86fc84e47546623eadd7ad

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => 'e6ad0ccb76589f5893c8');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => 'e6ad0ccb76589f5893c8');
