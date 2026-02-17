309c59458a3f09926dacc161286f9396bda6ace3
anomiex <anomiex@users.noreply.github.com>
2026-02-17 21:52:41 +0000

    Update dependency webpack to v5.105.2 (#47173)
    * Update dependency webpack to v5.105.2
    * Bump indirect ajv dep
    * i18n-check-webpack-plugin: Update test snapshots
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/22116548566
    
    Upstream-Ref: Automattic/jetpack@9b98a6bd531c45ce5282ee4575a7f817fb8e016e

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => 'cccef38973af06f95514');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '97f019b47fd816e65f05');
