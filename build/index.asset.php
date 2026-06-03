82fb53debdb1527d0432b8d91227e6b8a9414fcc
anomiex <anomiex@users.noreply.github.com>
2026-06-03 19:13:11 +0000

    Update Eslint to v10 (major) (#49352)
    * Update Eslint to v10
    * Hack around peer dep issues
    * Remove obsolete `v10_config_lookup_from_file` flag
    * eslint-changed: Drop obsolete test
    * eslint-changed: Update tests for removed `nodeType` field
    * Use `fixupPluginRules` from `@eslint/compat` to make `eslint-plugin-import` and `eslint-plugin-jest-dom` work.
    * Fix `no-useless-assignment`
      Mostly where all branches of an `if` or `switch` assign the variable (or
      return/throw), or a `let` outside of a `try` where the catch
      returns/re-throws, but a few are more complex.
    * Fix `preserve-caught-error`
    * Fix `no-unassigned-vars`
      One seems to have been trying for compat with ancient browsers that lack
      `undefined`, and the other seems to have been a bug.
    * Fix `no-shadow`
      The one instance seems intentional for naming.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/26906375256
    
    Upstream-Ref: Automattic/jetpack@c4d510012fd80873bca6d9264ff2d1b45e89cca6

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '97f019b47fd816e65f05');
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => 'bacee6a0479c3bedd25f');
