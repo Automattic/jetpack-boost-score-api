3a37753b9c573f60c7c97c6fd85e8854ef90c559
anomiex <anomiex@users.noreply.github.com>
2026-06-09 16:23:59 +0000

    Lock file maintenance (#49448)
    * Lock file maintenance
    * Remove trustPolicyExclude for reselect@5.1.1 after upgrade to 5.2.0.
    * stylelint: Remove unnecessary escapes in regex
    * Downgrade `@tanstack/react-router` to 1.167.1
      Avoid https://href.li/?https://github.com/TanStack/router/issues/7538
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27219400812
    
    Upstream-Ref: Automattic/jetpack@fa034e91a7375f7ab0b46f171abe539e3cb47c30

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => 'bacee6a0479c3bedd25f');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'bacee6a0479c3bedd25f');
